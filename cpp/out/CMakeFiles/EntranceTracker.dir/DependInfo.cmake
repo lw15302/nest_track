5cf0e97d2de28121adc782f225125c79360f0670
Liam White <lw15302@my.bristol.ac.uk>
2016-08-08 11:31:21 +0100

    reoraganised file structure to allow for java code

---
+++ cpp/out/CMakeFiles/EntranceTracker.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liam/Documents/Project/github/nest_track/cpp/src/display/VideoPlayer.cpp" "/home/liam/Documents/Project/github/nest_track/cpp/out/CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/display/VideoPlayer.cpp.o"
+  "/home/liam/Documents/Project/github/nest_track/cpp/src/main/Main.cpp" "/home/liam/Documents/Project/github/nest_track/cpp/out/CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/main/Main.cpp.o"
+  "/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracker.cpp" "/home/liam/Documents/Project/github/nest_track/cpp/out/CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracker.cpp.o"
+  "/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracking.cpp" "/home/liam/Documents/Project/github/nest_track/cpp/out/CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracking.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../include/main"
+  "../include/display"
+  "../include/track"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cpp/out/CMakeFiles/EntranceTracker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/main/Main.cpp.o"
+  "CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/display/VideoPlayer.cpp.o"
+  "CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracking.cpp.o"
+  "CMakeFiles/EntranceTracker.dir/home/liam/Documents/Project/github/nest_track/cpp/src/track/Tracker.cpp.o"
+  "EntranceTracker.pdb"
+  "EntranceTracker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EntranceTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
